e4082c2495df8a2f61741c58c29dcd4c4da04052
aydinsenih <aydinsenih@gmail.com>
2021-01-19 13:13:23 -0800

    redux implemented.

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { delFeature } from "../actions/featuresActions";
+import { connect } from "react-redux";
 
-const AddedFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
-  );
+const AddedFeature = (props) => {
+    return (
+        <li>
+            {/* Add an onClick to run a function to remove a feature */}
+            <button
+                className="button"
+                onClick={() => props.delFeature(props.feature)}
+            >
+                X
+            </button>
+            {props.feature.name}
+        </li>
+    );
 };
-
-export default AddedFeature;
+const mapStateToProps = (state) => {
+    return state;
+};
+export default connect(mapStateToProps, { delFeature })(AddedFeature);
